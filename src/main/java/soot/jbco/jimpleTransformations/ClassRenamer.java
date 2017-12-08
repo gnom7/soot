eaa2c3c5c2126eba3ecb41b8b83aa37605d6080a
gnom7 <system.out.println@mail.ru>
2017-12-08 18:08:18 +0300

    fix: handle ClassConstants in ClassRenamer

--- src/main/java/soot/jbco/jimpleTransformations/ClassRenamer.java
+++ src/main/java/soot/jbco/jimpleTransformations/ClassRenamer.java
@@ -24,6 +24,8 @@ import java.util.*;
 import soot.*;
 import soot.jbco.IJbcoTransform;
 import soot.jbco.util.*;
+import soot.jimple.ClassConstant;
+
 /**
  * @author Michael Batchelder
  *
@@ -71,12 +73,8 @@ public class ClassRenamer extends SceneTransformer  implements IJbcoTransform {
       }
 
       String oldName = c.getName();
-      String newName = oldToNewClassNames.get(oldName);
-      if (newName == null)
-      {
-        newName = getNewName(getNamePrefix(oldName));
-        oldToNewClassNames.put(oldName, newName);
-      }
+      String newName = getNewName(getNamePrefix(oldName));
+      oldToNewClassNames.put(oldName, newName);
 
       c.setName(newName);
       RefType crt = RefType.v(newName);
@@ -118,8 +116,16 @@ public class ClassRenamer extends SceneTransformer  implements IJbcoTransform {
          Iterator<ValueBox> udbIt = u.getUseAndDefBoxes().iterator();
           while (udbIt.hasNext())
           {
-            Value v = udbIt.next().getValue();
-            if (v instanceof soot.jimple.Ref)
+            ValueBox vb = udbIt.next();
+            Value v = vb.getValue();
+            if (v instanceof soot.jimple.ClassConstant)
+            {
+                ClassConstant constant = (ClassConstant) v;
+                RefType type = (RefType) constant.toSootType();
+                RefType updatedType = type.getSootClass().getType();
+                vb.setValue(ClassConstant.fromType(updatedType));
+            }
+            else if (v instanceof soot.jimple.Ref)
             {
               if (v.getType() instanceof soot.RefType)
               {
